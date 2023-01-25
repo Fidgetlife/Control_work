2221217d702e6c44daed1f4ab04683d2796e29b1
Fidgetman <a@mail.ru>
2023-01-25 12:27:39 +0300

    папка с программой

---
+++ Work/Program.cs
@@ -0,0 +1,35 @@
+﻿Console.Write("Введите кол-во элементов массива:  ");
+int n = Convert.ToInt32(Console.ReadLine());
+string[] array = new string[n];
+
+int counter = 0;
+    
+for (int i = 0; i < n; i++)
+    {
+        Console.Write("Введите элемент массива:  ");
+        string element = Console.ReadLine();
+        if(element.Length < 4)
+        {
+            counter++;
+        }
+        array[i] = element;
+    }    
+
+string[] newArray = new string[counter];
+
+void PullArray()
+{   
+    int y = 0; 
+    for(int i = 0; i <= newArray.Length; i++)
+        {    
+            if(array[i].Length < 4)
+            {                
+               newArray[y] = array[i];
+               y++;                
+            }
+        }    
+}
+
+PullArray();
+Console.WriteLine(String.Join(" ", newArray));
+
